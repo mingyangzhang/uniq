fb4616779cf5564f7efc7bc323bb10ef7423a64b
zmy <zmyrzwd@gmail.com>
2016-10-20 15:54:00 -0700

    Update api_client.py

--- apis/nb/clients/api_client.py
+++ apis/nb/clients/api_client.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
+#pylint: skip-file
+# This source code is licensed under the Apache license found in the
+# LICENSE file in the root directory of this project.
+
+
 import inspect
 
-from uniq.apis.nb.exceptions import NBApiClientException
+from uniq.apis.exceptions import NBApiClientException
 
 
 class ApiClient(object):
